4e95b5d5e174706b60ab587ce0e7de0b8cbf5ea9
DarthJktu <DarthJktu@gmail.com>
2020-10-12 16:00:10 +0300

    sdas

--- src/js/main.js
+++ src/js/main.js
@@ -83,6 +83,10 @@ $(document).ready(function () {
       $('.footer__subscribe-input input[type="text"]').addClass('error');
     }
   });
+  $('.filter__category-name').click(function (e) {
+      $('.filter').toggleClass('open');
+
+  });
 
 
   //
